e2a95a77d5724ad64462d1808228736c105a904c
Farhan Nazir <nazirf3440@gmail.com>
2023-01-25 09:33:45 +0100

    Fix eslint issue (#3)
    Co-authored-by: farhan-nazir <farhan.nazir@ingka.com>

--- src/test/unit/App.test.tsx
+++ src/test/unit/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '../../App';
 
-describe('App Component', function () {
+describe('App Component', () => {
   test('Should have link element', () => {
     render(<App />);
     const linkElement = screen.getByRole('link', { name: /learn react/i });
