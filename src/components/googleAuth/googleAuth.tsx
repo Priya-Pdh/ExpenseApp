ca1e2b4be5ce8e7dea91020d71816f9ad6e0b6a8
Priya Pradhan <102596886+Priya-Pdh@users.noreply.github.com>
2023-02-06 11:57:54 +0100

    undo googleAuth component

--- src/components/googleAuth/googleAuth.tsx
+++ src/components/googleAuth/googleAuth.tsx
@@ -1,20 +1,13 @@
 /* eslint-disable no-console */
 import React from 'react';
 import { GoogleLogin } from '@react-oauth/google';
-import { useNavigate } from 'react-router-dom';
 
 export default function GoogleAuth() {
-  const navigate = useNavigate();
-  // function onClick= (e: React.SyntheticEvent)=> {
-  //   e.preventDefault();
-
-  // }
   return (
     <>
       <GoogleLogin
         onSuccess={(credentialResponse) => {
-          navigate('/dashboard');
-          // console.log(credentialResponse);
+          console.log(credentialResponse);
         }}
         onError={() => {
           console.log('Login Failed');
