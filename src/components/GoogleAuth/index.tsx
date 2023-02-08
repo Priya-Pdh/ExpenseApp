01decb3394d5a1e9e916143f745c9854c5ba1978
Priya Pradhan <102596886+Priya-Pdh@users.noreply.github.com>
2023-02-08 09:55:48 +0100

    added navigate function (#11)

--- src/components/GoogleAuth/index.tsx
+++ src/components/GoogleAuth/index.tsx
@@ -1,15 +1,20 @@
 /* eslint-disable no-console */
 import React from 'react';
 import { GoogleLogin } from '@react-oauth/google';
+import { useNavigate } from 'react-router-dom';
 
 export default function GoogleAuth() {
+  const navigate = useNavigate();
+
   return (
     <>
       <GoogleLogin
         onSuccess={(credentialResponse) => {
+          navigate('/dashboard');
           console.log(credentialResponse);
         }}
         onError={() => {
+          navigate('/');
           console.log('Login Failed');
         }}
       />
